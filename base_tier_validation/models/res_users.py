0f4859d06777b3ba73b5478be9e2e1b5a883012c
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-02-15 22:00:44 +0100

    [IMP] base_tier_validation: systray performance

--- base_tier_validation/models/res_users.py
+++ base_tier_validation/models/res_users.py
@@ -1,6 +1,7 @@
 # Copyright 2019 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from collections import defaultdict
 from odoo import api, fields, models, modules
 
 
@@ -11,40 +12,47 @@ class Users(models.Model):
 
     @api.model
     def review_user_count(self):
+        user_reviews = self.env["tier.review"].search(
+            [
+                ("id", "in", self.env.user.review_ids.ids),
+                ("status", "=", "pending"),
+                ("can_review", "=", True),
+            ],
+            order="id desc",
+            limit=1000,
+        )
+        user_reviews_by_record_by_model_name = defaultdict(lambda: defaultdict(lambda: self.env["tier.review"]))
+        for review in user_reviews:
+            record = self.env[review.model].browse(review.res_id)
+            user_reviews_by_record_by_model_name[review.model][record] += review
+        model_ids = list({self.env["ir.model"]._get(name).id for name in user_reviews_by_record_by_model_name.keys()})
         user_reviews = {}
-        domain = [
-            ("status", "=", "pending"),
-            ("can_review", "=", True),
-            ("id", "in", self.env.user.review_ids.ids),
-        ]
-        review_groups = self.env["tier.review"].read_group(domain, ["model"], ["model"])
-        for review_group in review_groups:
-            model = review_group["model"]
-            reviews = self.env["tier.review"].search(review_group.get("__domain"))
-            if reviews:
-                records = (
-                    self.env[model]
-                    .with_user(self.env.user)
-                    .search([("id", "in", reviews.mapped("res_id"))])
-                    .filtered(lambda x: not x.rejected and x.can_review)
-                )
-                # Excludes any cancelled records depending on the structure of the model
-                if self.env[model]._state_field in self.env[model]._fields:
-                    records = records.filtered(
-                        lambda x: x[x._state_field] != x._cancel_state
-                    )
-                for rec in records:
-                    record = self.env[model]
-                    user_reviews[model] = {
-                        "id": rec.id,
-                        "name": record._description,
-                        "model": model,
-                        "active_field": "active" in record._fields,
-                        "icon": modules.module.get_module_icon(record._original_module),
-                        "type": "tier_review",
-                        "pending_count": len(records),
-                    }
+        for model_name, user_reviews_by_record in user_reviews_by_record_by_model_name.items():
+            domain = [("id", "in", list({r.id for r in user_reviews_by_record.keys()}))]
+            Model = self.env[model_name]
+            allowed_records = Model.search(domain)
+            if not allowed_records:
+                continue
+            module = Model._original_module
+            icon = module and modules.module.get_module_icon(module)
+            model = self.env["ir.model"]._get(model_name).with_prefetch(model_ids)
+            user_reviews[model_name] = {
+                "id": model.id,
+                "name": model.name,
+                "model": model_name,
+                "active_field": "active" in model._fields,
+                "icon": icon,
+                "type": "tier_review",
+                "pending_count": 0,
+            }
+            for record, review in user_reviews_by_record.items():
+                if record not in allowed_records:
+                    continue
+                for user_review in review:
+                    user_reviews[model_name]["pending_count"] += 1
         return list(user_reviews.values())
+        # todo add test https://github.com/odoo/odoo/commit/a3260cfcd75f2804cf6aec916fdb159cddcca74f
+
 
     @api.model
     def get_reviews(self, data):
